7a22c718186fbb2ac0ddaa04ca649132263b3280
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-10-20 04:22:22 -0500

    feat: :sparkles: Instalation of tailwind and some test for the react

---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+export function Header () {
+    return (
+        <div className="w-[100vws]">
+            <div className="h-[45vh] w-full overflow-hidden relative">
+                <img src="../../public/headerImg.png" alt="" className="w-full object-cover absolute" />
+            </div>
+        </div>
+    )
+}
